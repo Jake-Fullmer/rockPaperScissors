a2d00c6d236d248a5a8ba862f2030afea7c3d0ea
Jake-Fullmer <jnfu@outlook.com>
2020-01-29 15:54:20 -0700

    working single func

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
 let choices = ["rock", "paper", "scissors"];
 
-function rock() {
+function play(num) {
   let computerPick = choices[Math.floor(Math.random() * choices.length)];
-  console.log("This is computer choice within function", computerPick)
-  if (computerPick == choices[0]) {
-    document.getElementById("cardTopText").textContent = "It is a draw"
-    console.log(computerPick)
-  } else if (computerPick == choices[1]) {
-    document.getElementById("cardTopText").textContent = "You lose"
-    console.log(computerPick)
-  } else if (computerPick == choices[2]) {
+  let playerPick = choices[num];
+  console.log("Comp choice", computerPick);
+  console.log("Player choice", playerPick);
+  if (playerPick > computerPick) {
     document.getElementById("cardTopText").textContent = "You win"
+  } else if (playerPick < computerPick) {
+    document.getElementById("cardTopText").textContent = "You lose"
+  } else if (playerPick == computerPick) {
+    document.getElementById("cardTopText").textContent = "It is a draw"
   }
 }
-function paper() {
-  let computerPick = choices[Math.floor(Math.random() * choices.length)];
-  if (computerPick == choices[1]) {
-    document.getElementById("cardTopText").textContent = "It is a draw"
-    console.log(computerPick)
-  } else if (computerPick == choices[2]) {
-    document.getElementById("cardTopText").textContent = "You lose"
-    console.log(computerPick)
-  } else if (computerPick == choices[0]) {
-    document.getElementById("cardTopText").textContent = "You win"
-  }
-}
-function scissors() {
-  let computerPick = choices[Math.floor(Math.random() * choices.length)];
-  if (computerPick == choices[2]) {
-    document.getElementById("cardTopText").textContent = "It is a draw"
-    console.log(computerPick)
-  } else if (computerPick == choices[0]) {
-    document.getElementById("cardTopText").textContent = "You lose"
-    console.log(computerPick)
-  } else if (computerPick == choices[1]) {
-    document.getElementById("cardTopText").textContent = "You win"
-  }
-}
+
+// function rock() {
+//   let computerPick = choices[Math.floor(Math.random() * choices.length)];
+//   console.log("This is computer choice within function", computerPick)
+//   if (computerPick == choices[0]) {
+//     document.getElementById("cardTopText").textContent = "It is a draw"
+//     console.log(computerPick)
+//   } else if (computerPick == choices[1]) {
+//     document.getElementById("cardTopText").textContent = "You lose"
+//     console.log(computerPick)
+//   } else if (computerPick == choices[2]) {
+//     document.getElementById("cardTopText").textContent = "You win"
+//   }
+// }
+// function paper() {
+//   let computerPick = choices[Math.floor(Math.random() * choices.length)];
+//   if (computerPick == choices[1]) {
+//     document.getElementById("cardTopText").textContent = "It is a draw"
+//     console.log(computerPick)
+//   } else if (computerPick == choices[2]) {
+//     document.getElementById("cardTopText").textContent = "You lose"
+//     console.log(computerPick)
+//   } else if (computerPick == choices[0]) {
+//     document.getElementById("cardTopText").textContent = "You win"
+//   }
+// }
+// function scissors() {
+//   let computerPick = choices[Math.floor(Math.random() * choices.length)];
+//   if (computerPick == choices[2]) {
+//     document.getElementById("cardTopText").textContent = "It is a draw"
+//     console.log(computerPick)
+//   } else if (computerPick == choices[0]) {
+//     document.getElementById("cardTopText").textContent = "You lose"
+//     console.log(computerPick)
+//   } else if (computerPick == choices[1]) {
+//     document.getElementById("cardTopText").textContent = "You win"
+//   }
+// }
