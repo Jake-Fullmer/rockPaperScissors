bef2b8c72a226144054a9ce0dca3ed28e3603d3f
Jake-Fullmer <jnfu@outlook.com>
2020-01-29 14:59:57 -0700

    changing to single user func

---
+++ app.js
@@ -0,0 +1,45 @@
+let choices = ["rock", "paper", "scissors"];
+
+// let computer() {
+// let computerPickIndex = 
+let computerPick = Math.floor(Math.random() * choices.length);
+console.log("This is the computer choice index", computerPick);
+// }
+
+
+function rock() {
+  // computer()
+  if (computerPick == choices[0]) {
+    document.getElementById("cardTopText").textContent = "It is a draw"
+    console.log(computerPick)
+  } else if (computerPick == choices[1]) {
+    document.getElementById("cardTopText").textContent = "You lose"
+    console.log(computerPick)
+  } else if (computerPick == choices[2]) {
+    document.getElementById("cardTopText").textContent = "You win"
+  }
+}
+function paper() {
+  // computer()
+  if (computerPick == choices[1]) {
+    document.getElementById("cardTopText").textContent = "It is a draw"
+    console.log(computerPick)
+  } else if (computerPick == choices[2]) {
+    document.getElementById("cardTopText").textContent = "You lose"
+    console.log(computerPick)
+  } else if (computerPick == choices[0]) {
+    document.getElementById("cardTopText").textContent = "You win"
+  }
+}
+function scissors() {
+  // computer()
+  if (computerPick == choices[2]) {
+    document.getElementById("cardTopText").textContent = "It is a draw"
+    console.log(computerPick)
+  } else if (computerPick == choices[0]) {
+    document.getElementById("cardTopText").textContent = "You lose"
+    console.log(computerPick)
+  } else if (computerPick == choices[1]) {
+    document.getElementById("cardTopText").textContent = "You win"
+  }
+}
