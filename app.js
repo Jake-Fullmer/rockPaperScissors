2c3c47ad494b48779afc8a0d76093e5f69c915e2
Jake-Fullmer <jnfu@outlook.com>
2020-01-29 20:42:34 -0700

    syntax

--- app.js
+++ app.js
@@ -6,48 +6,49 @@ function play(num) {
   console.log("Comp choice", computerPick);
   console.log("Player choice", playerPick);
   if (playerPick > computerPick) {
-    document.getElementById("cardTopText").textContent = "You win"
+    document.getElementById("cardTopText").textContent = "You win";
   } else if (playerPick < computerPick) {
-    document.getElementById("cardTopText").textContent = "You lose"
+    document.getElementById("cardTopText").textContent = "You lose";
   } else if (playerPick == computerPick) {
-    document.getElementById("cardTopText").textContent = "It is a draw"
-  }
+    document.getElementById("cardTopText").textContent = "It is a draw";
+  };
 }
 
+// 1st attempt with multiple functions
 // function rock() {
 //   let computerPick = choices[Math.floor(Math.random() * choices.length)];
-//   console.log("This is computer choice within function", computerPick)
+//   console.log("This is computer choice within function", computerPick);
 //   if (computerPick == choices[0]) {
-//     document.getElementById("cardTopText").textContent = "It is a draw"
-//     console.log(computerPick)
+//     document.getElementById("cardTopText").textContent = "It is a draw";
+//     console.log(computerPick);
 //   } else if (computerPick == choices[1]) {
-//     document.getElementById("cardTopText").textContent = "You lose"
-//     console.log(computerPick)
+//     document.getElementById("cardTopText").textContent = "You lose";
+//     console.log(computerPick);
 //   } else if (computerPick == choices[2]) {
-//     document.getElementById("cardTopText").textContent = "You win"
+//     document.getElementById("cardTopText").textContent = "You win";
 //   }
 // }
 // function paper() {
 //   let computerPick = choices[Math.floor(Math.random() * choices.length)];
 //   if (computerPick == choices[1]) {
-//     document.getElementById("cardTopText").textContent = "It is a draw"
-//     console.log(computerPick)
+//     document.getElementById("cardTopText").textContent = "It is a draw";
+//     console.log(computerPick);
 //   } else if (computerPick == choices[2]) {
-//     document.getElementById("cardTopText").textContent = "You lose"
-//     console.log(computerPick)
+//     document.getElementById("cardTopText").textContent = "You lose";
+//     console.log(computerPick);
 //   } else if (computerPick == choices[0]) {
-//     document.getElementById("cardTopText").textContent = "You win"
+//     document.getElementById("cardTopText").textContent = "You win";
 //   }
 // }
 // function scissors() {
 //   let computerPick = choices[Math.floor(Math.random() * choices.length)];
 //   if (computerPick == choices[2]) {
-//     document.getElementById("cardTopText").textContent = "It is a draw"
-//     console.log(computerPick)
+//     document.getElementById("cardTopText").textContent = "It is a draw";
+//     console.log(computerPick);
 //   } else if (computerPick == choices[0]) {
-//     document.getElementById("cardTopText").textContent = "You lose"
-//     console.log(computerPick)
+//     document.getElementById("cardTopText").textContent = "You lose";
+//     console.log(computerPick);
 //   } else if (computerPick == choices[1]) {
-//     document.getElementById("cardTopText").textContent = "You win"
+//     document.getElementById("cardTopText").textContent = "You win";
 //   }
 // }
